c9964edb8fb4a6bbda26d52da7e006ed9ef9e19c
Phuc <hoangphucythus@gmail.com>
2023-11-09 11:44:29 +0700

    hust

--- src/Lab02/topic1.java
+++ src/Lab02/topic1.java
@@ -2,6 +2,7 @@ package Lab02;
 
 public class topic1 {
     public static void main(String[] args) {
+        System.out.println("adkfnjkadfkasdnfakjsdfn");
         System.out.println("kafkjnnkjad");
     }
 }
