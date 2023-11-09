e79e02126065e01c9265e095cf2e4ace00d9b943
Phuc <hoangphucythus@gmail.com>
2023-11-09 11:41:39 +0700

    modify from master

--- src/Lab02/topic1.java
+++ src/Lab02/topic1.java
@@ -2,6 +2,6 @@ package Lab02;
 
 public class topic1 {
     public static void main(String[] args) {
-        System.out.println("kafkjnjasdkfnkjad");
+        System.out.println("kafkjnnkjad");
     }
 }
